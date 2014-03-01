ce42ca6b6183d69422880d5e1bc373929b7384f2
standa255 <standa255@gmail.com>
2014-03-01 10:43:47 -0500

    third update of ex08_5.dart

--- bin/ex08_5.dart
+++ bin/ex08_5.dart
@@ -2,13 +2,13 @@ void main(){
   print("------------------------");  
   print("Solving puzzles problem:");  
   print("------------------------"); 
-  print ('''I don't understand what changes I should do concerning your code.'''); //your code: https://github.com/dzenanr/sliding_puzzle_wout_ui
-  print ('''Furthermore "an efficient method for solving sliding-block puzzles would yield the most important result in computer science of all time"[1].''');
-//[1] HEARN, Robert A. The Complexity of Sliding-Block Puzzles and Plank Puzzles. MIT computer science and artificial intelligence laboratory [online]. Available from: http://groups.csail.mit.edu/mac/users/bob/sliding-blocks.pdf
+  print ('''I don't understand what changes I should do concerning your code[1].'''); //[1] https://github.com/dzenanr/sliding_puzzle_wout_ui
+  print ('''Furthermore "an efficient method for solving sliding-block puzzles would yield the most important result in computer science of all time"[2].''');
+//[2] HEARN, Robert A. The Complexity of Sliding-Block Puzzles and Plank Puzzles. MIT computer science and artificial intelligence laboratory [online]. Available from: http://groups.csail.mit.edu/mac/users/bob/sliding-blocks.pdf
   print(" ");
   
   print("So I am not able to write a code which will solve the sliding-puzzles problem.");
-  print("But I found some hints how to write such an algorithm[2]."); //[2]http://entertainment.howstuffworks.com/puzzles/sliding-puzzles3.htm
+  print("But I found some hints how to write such an algorithm[3]."); //[3]http://entertainment.howstuffworks.com/puzzles/sliding-puzzles3.htm
   print("(1) Begin by maneuvering the 1 and 2 into their proper positions in the upper left corner.");
   print("(2) Position the 3 in the upper right corner.");
   print("(3) Maneuver the 4 under the 3");
