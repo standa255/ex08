dc6b44c22d96965dd7701773ab4ad4e196c48ff8
standa255 <standa255@gmail.com>
2014-03-01 10:41:16 -0500

    second update of ex08_5.dart

---
+++ bin/ex08_5.dart
@@ -0,0 +1,20 @@
+void main(){
+  print("------------------------");  
+  print("Solving puzzles problem:");  
+  print("------------------------"); 
+  print ('''I don't understand what changes I should do concerning your code.'''); //your code: https://github.com/dzenanr/sliding_puzzle_wout_ui
+  print ('''Furthermore "an efficient method for solving sliding-block puzzles would yield the most important result in computer science of all time"[1].''');
+//[1] HEARN, Robert A. The Complexity of Sliding-Block Puzzles and Plank Puzzles. MIT computer science and artificial intelligence laboratory [online]. Available from: http://groups.csail.mit.edu/mac/users/bob/sliding-blocks.pdf
+  print(" ");
+  
+  print("So I am not able to write a code which will solve the sliding-puzzles problem.");
+  print("But I found some hints how to write such an algorithm[2]."); //[2]http://entertainment.howstuffworks.com/puzzles/sliding-puzzles3.htm
+  print("(1) Begin by maneuvering the 1 and 2 into their proper positions in the upper left corner.");
+  print("(2) Position the 3 in the upper right corner.");
+  print("(3) Maneuver the 4 under the 3");
+  print("(4) Now slide the 3 to the left and the 4 up; the first row is done.");
+  print("(5) Repeat this process with next row, leaving row above intact.");
+  print("(6) Complete the puzzle by rearranging the pieces in the last two rows until they are in position.");
+  print('''(7) Sometimes it's easier to create "snakes" of pieces in the proper order rather than put them in their final position individually.''');
+  print("(8) If you're assembling a picture, study the design carefully before it's scrambled. It's harder to solve if you're not clear about the final outcome.");
+  }
